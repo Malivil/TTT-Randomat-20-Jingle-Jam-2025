1723a9bee4696842fcbf8dc53982d128c9ce3cef
Malivil <mlebson@outlook.com>
2026-02-26 17:39:26 -0500

    WIP Cosmic Clones

--- lua/randomat2/events/cosmicclones.lua
+++ lua/randomat2/events/cosmicclones.lua
@@ -14,7 +14,8 @@ local EVENT = {}
 EVENT.Title = "Cosmic Clones"
 EVENT.id = "cosmicclones"
 
-CreateConVar("randomat_cosmicclones_delay", 5, FCVAR_NONE, "How long the delay should be between a user moving and their clone doing the same movement", 1, 60)
+CreateConVar("randomat_cosmicclones_delay", 5, FCVAR_NONE, "How long (in seconds) the delay should be between a user moving and their clone doing the same movement", 1, 60)
+CreateConVar("randomat_cosmicclones_rate", 0.5, FCVAR_NONE, "How often (in seconds) the player's data should be recorded. Lower number is more accurate, but higher compute requirements")
 
 local dark_red = Color(136, 0, 0)
 local function CreateClone(ply, pos, ang)
@@ -36,8 +37,10 @@ local function CreateClone(ply, pos, ang)
 end
 
 local mv_start = {}
+local mv_last = {}
 function EVENT:Begin()
     local delay = GetConVar("randomat_cosmicclones_delay"):GetInt()
+    local rate = GetConVar("randomat_cosmicclones_rate"):GetInt()
 
     -- The clones are invincible
     self:AddHook("EntityTakeDamage", function(target, dmginfo)
@@ -48,79 +51,100 @@ function EVENT:Begin()
         end
     end)
 
-    self:AddHook("SetupMove", function(ply, mv, cmd)
-        local curTime = CurTime()
+    -- Destroy clones and reset state when a player dies
+    -- This means clones will be recreated automatically if
+    -- a player is respawned
+    self:AddHook("PostPlayerDeath", function(ply)
+        if not ply.RdmtCosmicClones then return end
+
+        for _, c in ipairs(ply.RdmtCosmicClones) do
+            SafeRemoveEntity(c)
+        end
+        ply.RdmtCosmicClones = nil
+
         local sid64 = ply:SteamID64()
+        mv_start[sid64] = nil
+        timer.Remove("RdmtCosmicCloneCreate_" .. sid64)
+    end)
 
-        local speed = ply:GetWalkSpeed()
-        if ply:IsWalking() then
-            speed = ply:GetSlowWalkSpeed()
-        end
-        if ply:Crouching() then
-            speed = speed * ply:GetCrouchedWalkSpeed()
-        end
+    self:AddHook("Think", function()
+        for _, ply in ipairs(self:GetAlivePlayers()) do
+            -- TODO: Remove
+            if ply:Nick() ~= "Malivil" then continue end
 
-        -- TODO: What else do we need from this?
-        -- TODO: Jumping?
-        local mvData = {
-            pos = ply:GetPos(),
-            ang = ply:GetAngles(),
-            crouching = ply:Crouching(),
-            walking = ply:IsWalking(),
-            speed = speed,
-            weapon = {}
-            --view = cmd:GetViewAngles(),
-            --btns = mv:GetButtons(),
-            --mvAng = mv:GetMoveAngles(),
-            --fwd = mv:GetForwardSpeed(),
-            --side = mv:GetSideSpeed(),
-            --up = mv:GetUpSpeed(),
-        }
+            local curTime = CurTime()
+            local sid64 = ply:SteamID64()
+            -- Don't update too often
+            if mv_last[sid64] and (curTime - mv_last[sid64]) < rate then continue end
 
-        local activeWep = ply:GetActiveWeapon()
-        if IsValid(activeWep) and activeWep ~= NULL then
-            mvData.weapon.model = activeWep:GetModel()
-            mvData.weapon.holdType = activeWep:GetHoldType()
-        end
-
-        -- This player already has one or more clones, update them
-        if mv_start[sid64] == false then
-            if ply.RdmtCosmicClones then
-                for _, c in ipairs(ply.RdmtCosmicClones) do
-                    c:AddMoveData(curTime, mvData)
-                end
-            else
-                mv_start[sid64].moves[curTime] = mvData
+            local speed = ply:GetWalkSpeed()
+            if ply:IsWalking() then
+                speed = ply:GetSlowWalkSpeed()
             end
-            return
-        -- Start waiting to create the clone
-        elseif not mv_start[sid64] then
-            mv_start[sid64] = {
+            if ply:Crouching() then
+                speed = speed * ply:GetCrouchedWalkSpeed()
+            end
+
+            -- TODO: What else do we need from this?
+            -- TODO: Jumping?
+            local mvData = {
+                time = curTime,
                 pos = ply:GetPos(),
                 ang = ply:GetAngles(),
-                moves = {}
+                view = ply:EyeAngles(),
+                crouching = ply:Crouching(),
+                walking = ply:IsWalking(),
+                speed = speed,
+                weapon = {}
             }
+            mv_last[sid64] = curTime
 
-            timer.Create("RdmtCosmicCloneCreate_" .. sid64, delay, 1, function()
-                if not IsValid(ply) then return end
-                local mvStart = mv_start[sid64]
+            local activeWep = ply:GetActiveWeapon()
+            if IsValid(activeWep) and activeWep ~= NULL then
+                mvData.weapon.model = activeWep:GetModel()
+                mvData.weapon.holdType = activeWep:GetHoldType()
+            end
 
-                -- Create the clone and pass any move history that we have
-                local clone = CreateClone(ply, mvStart.pos, mvStart.ang)
-                for t, d in pairs(mv_start[sid64].moves) do
-                    clone:AddMoveData(t, d)
+            -- This player already has one or more clones, update them
+            if mv_start[sid64] == false then
+                if ply.RdmtCosmicClones then
+                    for _, c in ipairs(ply.RdmtCosmicClones) do
+                        c:AddMoveData(mvData)
+                    end
+                else
+                    TableInsert(mv_start[sid64].moves, mvData)
                 end
+            -- Start waiting to create the clone
+            elseif not mv_start[sid64] then
+                mv_start[sid64] = {
+                    pos = ply:GetPos(),
+                    ang = ply:GetAngles(),
+                    moves = {
+                        [1] = mvData
+                    }
+                }
 
-                mv_start[sid64] = false
+                timer.Create("RdmtCosmicCloneCreate_" .. sid64, delay, 1, function()
+                    if not IsValid(ply) then return end
+                    local mvStart = mv_start[sid64]
 
-                if not ply.RdmtCosmicClones then
-                    ply.RdmtCosmicClones = {}
-                end
-                TableInsert(ply.RdmtCosmicClones, clone)
-            end)
-        -- Keep track of any move data while the clone is being created
-        else
-            mv_start[sid64].moves[curTime] = mvData
+                    -- Create the clone and pass any move history that we have
+                    local clone = CreateClone(ply, mvStart.pos, mvStart.ang)
+                    for _, d in ipairs(mv_start[sid64].moves) do
+                        clone:AddMoveData(d)
+                    end
+
+                    mv_start[sid64] = false
+
+                    if not ply.RdmtCosmicClones then
+                        ply.RdmtCosmicClones = {}
+                    end
+                    TableInsert(ply.RdmtCosmicClones, clone)
+                end)
+            -- Keep track of any move data while the clone is being created
+            else
+                TableInsert(mv_start[sid64].moves, mvData)
+            end
         end
     end)
 end
