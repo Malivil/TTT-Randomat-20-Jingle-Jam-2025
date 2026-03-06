be5e0591f11d273bd6af605332916e8600a41e65
donaldsonnick <noxxflame@gmail.com>
2026-03-06 16:20:31 +0000

    Fixed clones flickering due to client/server desync

--- gamemodes/terrortown/entities/entities/cl_ttt_randomat_cosmicclones_clone.lua
+++ gamemodes/terrortown/entities/entities/cl_ttt_randomat_cosmicclones_clone.lua
@@ -1,9 +1,13 @@
 AddCSLuaFile()
 
+local ents = ents
 local math = math
+local pairs = pairs
 local table = table
 
+local EntsCreateClient = ents.CreateClientside
 local MathRound = math.Round
+local TableCount = table.Count
 local TableInsert = table.insert
 local TableRemove = table.remove
 
@@ -14,32 +18,27 @@ ENT.Base                 = "base_anim"
 
 if CLIENT then
     ENT.PrintName        = "Cosmic Clone"
+    ENT.PositionCallback = nil
 end
 
+ENT.CloneColor           = Color(136, 0, 0)
+ENT.CloneMaterial        = "!RdmtCosmicCloneMaterial"
 ENT.TickRate             = 0.1
 ENT.MaxTicks             = 10
 ENT.IsDead               = false
 
--- TODO: Sometimes Touch doesn't work
-
 function ENT:Initialize()
     self.TickRate = MathRound(engine.TickInterval(), 3)
-    self:SetMoveType(MOVETYPE_NONE)
-    self:SetSolid(SOLID_BBOX)
-    self:SetCollisionBounds(Vector(-16, -16, 0), Vector(16, 16, 84))
-    self:SetNoDraw(true)
-    if SERVER then
-        self:SetTrigger(true)
-    end
+    self:SetColor(self.CloneColor)
+    self:SetMaterial(self.CloneMaterial)
 end
 
 function ENT:SetupDataTables()
- 	self:NetworkVar("String", "CloneOf")
  	self:NetworkVar("Int", "Delay")
 end
 
 function ENT:SetNextThink(curTime)
-    self:NextThink(curTime)
+    self:SetNextClientThink(curTime)
     return true
 end
 
@@ -90,6 +89,16 @@ print(self:EntIndex(), curTime, curTime - (lastTick or 0), #self.MoveData, mvDat
     self.IsDead = false
     self:SetPos(mvData.pos)
     self:SetAngles(mvData.ang)
+    if self.PositionCallback then
+        self.PositionCallback(self, mvData.pos)
+    end
+    self:SetCycle(mvData.cyc)
+    self:SetSequence(mvData.seq)
+    -- TODO: This doesn't handle weapon attacking like swinging the crowbar
+    for pose, val in pairs(mvData.poses) do
+        self:SetPoseParameter(pose, val)
+    end
+    self:UpdateWeaponModel(mvData.wep)
 
     return self:SetNextThink(curTime)
 end
@@ -98,33 +107,90 @@ ENT.LastAdded = nil
 function ENT:AddMoveData(mvData)
     mvData.added = (mvData.added or 0) + 1
 
+    -- Don't allow duplicate values
     if self.LastAdded and not self.LastAdded.dead and not mvData.dead then
-        if self.LastAdded.pos:IsEqualTol(mvData.pos, 0) then return end
+        local samePos = self.LastAdded.pos:IsEqualTol(mvData.pos, 0)
+
+        if samePos and
+            self.LastAdded.ang:IsEqualTol(mvData.ang, 0) and
+            self.LastAdded.sec == mvData.sec and
+            self.LastAdded.cyc == mvData.cyc and
+            TableCount(self.LastAdded.poses) == TableCount(mvData.poses)
+        then
+            local allSame = true
+            for pose, val in pairs(mvData.poses) do
+                if not self.LastAdded.poses[pose] then
+                    allSame = false
+                    break
+                end
+                if self.LastAdded.poses[pose] ~= val then
+                    allSame = false
+                    break
+                end
+            end
+
+            if allSame then return end
+        end
     end
 
     self.LastAdded = mvData
     TableInsert(self.MoveData, mvData)
 end
 
-function ENT:StartTouch(ent)
-    if self.IsDead then return end
 
-    local damage = ent:Health()
-    local att = player.GetBySteamID64(self:GetCloneOf())
-    if not IsValid(att) or Randomat:ShouldActLikeJester(att) then
-        att = game.GetWorld()
-    else
-        -- Boost this enough to compensate for the attacker's karma
-        damage = damage * (1 + (1 - att:GetDamageFactor()))
+function ENT:OnRemove(fullUpdate)
+    SafeRemoveEntity(self.FakeWep)
+    self.FakeWep = nil
+end
+
+function ENT:UpdateWeaponModel(model)
+    if not model then return end
+
+    if not self.FakeWep then
+        local attachment
+        local lookup = self:LookupAttachment("anim_attachment_RH")
+        if lookup == 0 then
+            attachment = { Pos = self:GetPos() + self:OBBCenter() + Vector(0, 0, 5), Ang = self:GetForward():Angle() + Angle(20, 0, 0) }
+        else
+            attachment = self:GetAttachment(lookup)
+        end
+
+        -- Create the Fake weapon
+        self.FakeWep = EntsCreateClient("base_anim")
+        self.FakeWep:SetOwner(self)
+        self.FakeWep:AddEffects(EF_BONEMERGE)
+        self.FakeWep:SetMoveType(MOVETYPE_NONE)
+        self.FakeWep:SetPos(attachment.Pos)
+        self.FakeWep:SetAngles(attachment.Ang)
+        self.FakeWep:SetParent(self)
+        self.FakeWep:SetNoDraw(true)
     end
 
-    -- Kill whoever touches the clone
-    local dmginfo = DamageInfo()
-    dmginfo:SetAttacker(att)
-    dmginfo:SetInflictor(self)
-    dmginfo:SetDamagePosition(self:GetPos())
-    dmginfo:SetDamageType(DMG_DIRECT)
-    dmginfo:SetDamage(math.ceil(damage))
-    dmginfo:SetDamageForce(Vector(0, 0, 1))
-    ent:TakeDamageInfo(dmginfo)
+    if not util.IsValidModel(model) then
+        self.ValidFakeWep = false
+    else
+        self.ValidFakeWep = true
+
+        if self.FakeWep:GetModel() ~= model then
+            self.FakeWep:SetModel(model)
+        end
+
+        if self.FakeWep:GetColor() ~= self.CloneColor then
+            self.FakeWep:SetColor(self.CloneColor)
+        end
+
+        if self.FakeWep:GetMaterial() ~= self.CloneMaterial then
+            self.FakeWep:SetMaterial(self.CloneMaterial)
+        end
+    end
+end
+
+function ENT:Draw(flags)
+    if self.IsDead then return end
+
+    self:DrawModel(flags)
+
+    if self.ValidFakeWep then
+        self.FakeWep:DrawModel(flags)
+    end
 end
